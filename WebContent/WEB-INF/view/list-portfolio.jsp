25ead10e415da857e3fccf4ca7d1fa1f333655b1
wiltrahan <twilorip@gmail.com>
2018-04-19 14:47:37 -0400

    initial commit

---
+++ WebContent/WEB-INF/view/list-portfolio.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>List Portfolio</title>
+</head>
+<body>
+	Portfolio list ---- COMING SOON!!
+
+</body>
+</html>
---
+++ src/com/scrape/testdb/TestDbServlet.java
@@ -0,0 +1,52 @@
+package com.scrape.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//setup connection variables
+		String user = "portfolio";
+		String pass = "frankie1";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/portfolio-scraper?useSSL=false";
+		String driver = "com.mysql.jdbc.Driver";
+		
+		//get connection to database
+		try {
+			PrintWriter out = response.getWriter();
+			
+			out.println("Connecting to database: " + jdbcUrl);
+			Class.forName(driver);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			out.println("SUCCESS!! DO..YOU..BELEIVE..IT??");
+			
+			myConn.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+	}
+
+}
