e7d0537a1d0c3e1cf7e1af22220ebec53f874b0c
wiltrahan <twilorip@gmail.com>
2018-04-22 17:24:02 -0400

    date time entity class

--- src/com/scrape/portfolio/DateAndTime.java
+++ src/com/scrape/portfolio/DateAndTime.java
@@ -5,22 +5,67 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="date_time")
 public class DateAndTime {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
 	
-	public static void main(String args[]) throws ParseException {
-		
-		 String date_time = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-				.format(Calendar.getInstance()
-				.getTime());
-		 String[] timeSplit = date_time.split("\\s+");
-		 String time = formatTime(timeSplit[1]);
-		
-		System.out.println("Date: " + timeSplit[0]);
-		System.out.println("Time: " + time);
-		
+	@Column(name="date")
+	private String date;
+	
+	@Column(name="time")
+	private String time;
+	
+	@Column(name="date_time_id")
+	private int date_time_id;
+	
+	public DateAndTime() {
+	
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getDate_time_id() {
+		return date_time_id;
+	}
+
+	public void setDate_time_id(int date_time_id) {
+		this.date_time_id = date_time_id;
+	}
+
 	private static String formatTime(String time) throws ParseException {
 		//old format
 	    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
@@ -38,30 +83,11 @@ public class DateAndTime {
 
 }
 
-
-//private void stockInfo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ParseException {
+//String date_time = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+//.format(Calendar.getInstance()
+//.getTime());
+//String[] timeSplit = date_time.split("\\s+");
+//String time = formatTime(timeSplit[1]);
 //
-//    String date = request.getParameter("date");
-//    List<Stock> stocks = QuerydbUtil.getStocks(date);
-//
-//    String[] timeSplit = date.split("\\s+");
-//
-//    String time = formatTime(timeSplit[1]);
-//
-//    request.setAttribute("STOCKS_LIST", stocks);
-//    request.setAttribute("DATE", timeSplit[0]);
-//    request.setAttribute("TIME", time);
-//
-//    RequestDispatcher dispatcher = request.getRequestDispatcher("stock-info.jsp");
-//    dispatcher.forward(request, response);
-//}
-//
-//private String formatTime(String time) throws ParseException {
-//    //old format
-//    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-//    Date date3 = sdf.parse(time);
-//    //new format
-//    SimpleDateFormat sdf2 = new SimpleDateFormat("h:mm aa");
-//    //formatting the given time to new format with AM/PM
-//    return sdf2.format(date3);
-//}
+//System.out.println("Date: " + timeSplit[0]);
+//System.out.println("Time: " + time);
